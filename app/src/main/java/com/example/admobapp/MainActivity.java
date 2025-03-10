0b80b5838eae838c32c6ca43ac3d274e8185e080
phi quoc hung <phiquochung@vnpt.vn>
2025-03-10 11:13:22 +0700

    Google AdMob

---
+++ app/src/main/java/com/example/admobapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.admobapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+
+public class MainActivity extends AppCompatActivity {
+
+    private InterstitialAd mInterstitialAd;
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest,
+            new InterstitialAdLoadCallback() {
+                @Override
+                public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                    // The mInterstitialAd reference will be null until
+                    // an ad is loaded.
+                    mInterstitialAd = interstitialAd;
+                    Log.i(TAG, "onAdLoaded");
+
+                    mInterstitialAd.show(MainActivity.this);
+                }
+
+                @Override
+                public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                    // Handle the error
+                    Log.d(TAG, loadAdError.toString());
+                    mInterstitialAd = null;
+                }
+            });
+    }
+}
---
+++ app/src/main/java/com/example/admobapp/utils/Constants.java
@@ -0,0 +1,5 @@
+package com.example.admobapp.utils;
+
+public class Constants {
+    public static final String AD_UNIT_ID = "ca-app-pub-2473680839320459/5710879293";
+}
